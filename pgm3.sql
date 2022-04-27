7128b26e48f734ceceacba92f90c194c02c25a9e
LismyThomas <96164541+LismyThomas@users.noreply.github.com>
2022-04-27 15:25:30 +0530

    Add files via upload

---
+++ pgm3.sql
@@ -0,0 +1,16 @@
+use adbms
+DELIMITER //
+CREATE PROCEDURE sumofmton(in n int,in m int)
+BEGIN
+DECLARE i, final INT;
+SET i=n;
+select concat(n,m);
+SET final=0;
+label: while(i<m)do
+set final=final+1;
+set i=i+1;
+end while label;
+select concat('sum from',n,'to',m,'=',final);
+END //
+DELIMITER ;
+call sumofmton(5,10);
